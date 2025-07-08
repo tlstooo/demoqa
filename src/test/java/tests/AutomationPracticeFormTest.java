6286388d27a1ee6a1f31577cbab73175411f57d7
tlsto <frol54@gmail.com>
2025-07-08 15:31:40 +0300

    Upd AutomationPracticeFormTest

--- src/test/java/tests/AutomationPracticeFormTest.java
+++ src/test/java/tests/AutomationPracticeFormTest.java
@@ -2,7 +2,7 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
-import com.github.javafaker.Faker;
+import io.qameta.allure.*;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
@@ -11,8 +11,9 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import pages.RegistrationPage;
 import utils.RandomizeData;
-import java.util.Locale;
 import java.util.Map;
+
+import static com.codeborne.selenide.logevents.SelenideLogger.step;
 
 @Tag("automation-practice-form")
 public class AutomationPracticeFormTest {
@@ -39,71 +40,79 @@ public class AutomationPracticeFormTest {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 
+    @Test
     @DisplayName("Проверка всех полей на странице регистрации")
-    @Test
+    @Feature("Валидация полей регистрации")
+    @Owner("safrolov")
+    @Severity(SeverityLevel.NORMAL)
+    @Link(value = "DemoQA", url = "https://demoqa.com")
     public void successfulRegistrationTest() {
-        registrationPage.openPage()
-                .removeBanners()
-                .setFirstName(randomizeData.firstName)
-                .setLastName(randomizeData.lastName)
-                .setEmail(randomizeData.email)
-                .setGender(randomizeData.gender)
-                .setPhoneNumber(randomizeData.phone)
-                .setDateOfBirth(randomizeData.day, randomizeData.month, randomizeData.year)
-                .setSubjects(randomizeData.subject)
-                .setHobbies(randomizeData.hobbies)
-                .setUploadPic(randomizeData.pics)
-                .setCurrentAddress(randomizeData.currentAddress)
-                .setState(randomizeData.state)
-                .setCity(randomizeData.city)
-                .clickSubmitButton();
+        step("Открываем тестируемую страницу", () -> { registrationPage.openPage(); });
+        step("Удаляем рекламные баннеры", () -> { registrationPage.removeBanners(); });
+        step("Заполняем имя", () -> { registrationPage.setFirstName(randomizeData.firstName); });
+        step("Заполняем фамилию", () -> { registrationPage.setLastName(randomizeData.lastName); });
+        step("Заполняем email", () -> { registrationPage.setEmail(randomizeData.email); });
+        step("Заполняем пол", () -> { registrationPage.setGender(randomizeData.gender); });
+        step("Заполняем телефонный номер", () -> { registrationPage.setPhoneNumber(randomizeData.phone); });
+        step("Заполняем дату рождения", () -> { registrationPage.setDateOfBirth(randomizeData.day, randomizeData.month, randomizeData.year); });
+        step("Заполняем область", () -> { registrationPage.setSubjects(randomizeData.subject); });
+        step("Заполняем хобби", () -> { registrationPage.setHobbies(randomizeData.hobbies); });
+        step("Загружаем изображение", () -> { registrationPage.setUploadPic(randomizeData.pics); });
+        step("Заполняем актуальный адрес", () -> { registrationPage.setCurrentAddress(randomizeData.currentAddress); });
+        step("Заполняем штат", () -> { registrationPage.setState(randomizeData.state); });
+        step("Заполняем город", () -> { registrationPage.setCity(randomizeData.city); });
+        step("Нажимаем кнопку", () -> { registrationPage.clickSubmitButton(); });
 
-        registrationPage
-                .checkResult("Student Name", randomizeData.firstName + " " + randomizeData.lastName)
-                .checkResult("Student Email", randomizeData.email)
-                .checkResult("Gender", randomizeData.gender)
-                .checkResult("Mobile", randomizeData.phone)
-                .checkResult("Date of Birth", randomizeData.day + " " + months[Integer.parseInt(randomizeData.month)-1] + "," + randomizeData.year)
-                .checkResult("Subjects", randomizeData.subject)
-                .checkResult("Hobbies", randomizeData.hobbies)
-                .checkResult("Picture", randomizeData.pics)
-                .checkResult("Address", randomizeData.currentAddress)
-                .checkResult("State and City", randomizeData.state + " " + randomizeData.city);
+        step("Проверяем имя и фамилию", () -> {registrationPage.checkResult("Student Name", randomizeData.firstName + " " + randomizeData.lastName);});
+        step("Проверяем email", () -> {registrationPage.checkResult("Student Email", randomizeData.email);});
+        step("Проверяем пол", () -> {registrationPage.checkResult("Gender", randomizeData.gender);});
+        step("Проверяем номер телефона", () -> {registrationPage.checkResult("Phone Number", randomizeData.phone);});
+        step("Проверяем дату рождения", () -> {registrationPage.checkResult("Date of Birth", randomizeData.day + " " + months[Integer.parseInt(randomizeData.month)-1] + "," + randomizeData.year);});
+        step("Проверяем область", () -> {registrationPage.checkResult("Subjects", randomizeData.subject);});
+        step("Проверяем хобби", () -> {registrationPage.checkResult("Hobbies", randomizeData.hobbies);});
+        step("Проверяем изображение", () -> {registrationPage.checkResult("Picture", randomizeData.pics);});
+        step("Проверяем адрес", () -> {registrationPage.checkResult("Address", randomizeData.currentAddress);});
+        step("Проверяем штат и город", () -> {registrationPage .checkResult("State and City", randomizeData.state + " " + randomizeData.city);});
     }
 
+
+    @Test
     @DisplayName("Проверка номера недопустимой длины")
-    @Test
+    @Feature("Валидация полей регистрации")
+    @Owner("safrolov")
+    @Severity(SeverityLevel.NORMAL)
+    @Link(value = "DemoQA", url = "https://demoqa.com")
     public void negativeInputRegistrationTest() {
-        registrationPage
-                .openPage()
-                .removeBanners()
-                .setPhoneNumber(randomizeData.elevenDigitsPhoneNumber)
-                .clickSubmitButton();
+        step("Открываем тестируемую страницу", () -> { registrationPage.openPage();});
+        step("Удаляем рекламные баннеры", () -> { registrationPage.removeBanners(); });
+        step("Заполняем телефонный номер", () -> { registrationPage.setPhoneNumber(randomizeData.elevenDigitsPhoneNumber); });
+        step("Нажимаем кнопку", () -> { registrationPage.clickSubmitButton(); });
+        step("Проверяем телефонный номер", () -> { registrationPage
+                .checkNegativeInputResult("Phone Number", randomizeData.elevenDigitsPhoneNumber);});
 
-
-        registrationPage
-                .checkNegativeInputResult("Phone Number", randomizeData.elevenDigitsPhoneNumber);
     }
 
-    @DisplayName("Проверка заполнения только обязательных полей")
     @Test
+    @DisplayName("Проверка заполнения только обязательных полей")
+    @Feature("Валидация полей регистрации")
+    @Owner("safrolov")
+    @Severity(SeverityLevel.NORMAL)
+    @Link(value = "DemoQA", url = "https://demoqa.com")
     public void obligatoryFieldsTest()
     {
-        registrationPage
-                .openPage()
-                .removeBanners()
-                .setFirstName(randomizeData.firstName)
-                .setLastName(randomizeData.lastName)
-                .setGender(randomizeData.gender)
-                .setPhoneNumber(randomizeData.phone)
-                .clickSubmitButton();
+        step("Открываем тестируемую страницу", () -> { registrationPage.openPage(); });
+        step("Удаляем рекламные баннеры", () -> { registrationPage.removeBanners(); });
+        step("Заполняем имя", () -> { registrationPage.setFirstName(randomizeData.firstName); });
+        step("Заполняем фамилию", () -> { registrationPage.setLastName(randomizeData.lastName); });
+        step("Заполняем пол", () -> { registrationPage.setGender(randomizeData.gender); });
+        step("Заполняем телефонный номер", () -> { registrationPage.setPhoneNumber(randomizeData.phone); });
+        step("Нажимаем кнопку", () -> { registrationPage.clickSubmitButton(); });
 
-        registrationPage
-                .checkRadioColor("Gender","green")
-                .checkBorderColor("First Name","green")
-                .checkBorderColor("Last Name","green")
-                .checkResult("Student Name", randomizeData.firstName + " " + randomizeData.lastName)
-                .checkResult("Gender", randomizeData.gender)
-                .checkResult("Mobile", randomizeData.phone);
+        step("Проверяем выбраный пол на радиокнопке", () -> {registrationPage.checkRadioColor("Gender","green");});
+        step("Проверяем валидацию имени", () -> {registrationPage.checkBorderColor("First Name","green");});
+        step("Проверяем валидацию фамилии", () -> {registrationPage.checkBorderColor("Last Name","green");});
+        step("Проверяем имя и фамилию", () -> {registrationPage.checkResult("Student Name", randomizeData.firstName + " " + randomizeData.lastName);});
+        step("Проверяем пол", () -> {registrationPage.checkResult("Gender", randomizeData.gender);});
+        step("Проверяем телефонный номер", () -> {registrationPage.checkResult("Mobile", randomizeData.phone);});
     }
 }
