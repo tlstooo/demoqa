de14a41856cae0cff9b1cc8cc7d88f8106d50cf5
tlsto <frol54@gmail.com>
2025-07-13 22:37:17 +0300

    Upd RestAssuredTests

--- src/test/java/tests/RestAssuredTests.java
+++ src/test/java/tests/RestAssuredTests.java
@@ -1,19 +1,19 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.logevents.SelenideLogger;
-import helpers.Attach;
 import io.qameta.allure.*;
-import io.qameta.allure.selenide.AllureSelenide;
+
 import io.restassured.RestAssured;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.*;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
-import java.util.Map;
+import com.github.javafaker.Faker;
 
-import static com.codeborne.selenide.logevents.SelenideLogger.step;
+import java.util.Locale;
+
 import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.is;
 
 @Tag("reqresTest")
 public class RestAssuredTests {
@@ -25,24 +25,6 @@ public class RestAssuredTests {
     @BeforeAll
     static void beforeAll() {
         RestAssured.baseURI = "https://reqres.in/";
-
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
-                "enableVNC", true,
-                "enableVideo", true
-        ));
-        Configuration.browserCapabilities = capabilities;
-
-        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-    }
-
-    @AfterEach
-    void afterEach() {
-        Attach.screenshotAs("Last Screenshot");
-        Attach.pageSource();
-        Attach.browserConsoleLogs();
-        Attach.getVideoUrl();
-        Attach.addVideo();
     }
 
     @DisplayName("Проверка счётчика пользователей")
@@ -52,7 +34,6 @@ public class RestAssuredTests {
     @Link(value = "reqres", url = "https://reqres.in")
     @Test
     void checkUserCount() {
-        step("Проверяем ручку /api/users?page=2", () -> {
             given()
                 .log().uri()
                 .get("/api/users?page=2")
@@ -61,6 +42,101 @@ public class RestAssuredTests {
                 .statusCode(200)
                 .log().body()
                 .body("total", Matchers.notNullValue());
-        });
     }
+
+    @DisplayName("Проверка получения данных пользователя")
+    @Feature("Проверка API")
+    @Owner("safrolov")
+    @Severity(SeverityLevel.NORMAL)
+    @Link(value = "reqres", url = "https://reqres.in")
+    @Test
+    void checkUserData() {
+        given()
+                .log().uri()
+                .get("/api/users/2")
+                .then()
+                .log().status()
+                .statusCode(200)
+                .log().body()
+                .body("data.id", is(2))
+                .body("data.email", Matchers.notNullValue())
+                .body("data.first_name", Matchers.notNullValue())
+                .body("data.last_name", Matchers.notNullValue())
+                .body("data.avatar", Matchers.notNullValue());
+    }
+
+    @DisplayName("Позитивная проверка авторизации")
+    @Feature("Проверка API")
+    @Owner("safrolov")
+    @Severity(SeverityLevel.NORMAL)
+    @Link(value = "reqres", url = "https://reqres.in")
+    @Test
+    void checkPositiveUserCredentials() {
+        String authData = "{\"email\": \"eve.holt@reqres.in\", \"password\": \"cityslicka\"}";
+        given()
+                .body(authData)
+                .header("x-api-key", "reqres-free-v1")
+                .contentType(JSON)
+                .log().uri()
+                .when()
+                .post("/api/login")
+                .then()
+                .log().status()
+                .statusCode(200)
+                .log().body()
+                .body("token", is("QpwL5tke4Pnpja7X4"));
+    }
+
+    @DisplayName("Проверка ошибки при отсутствии пароля")
+    @Feature("Проверка API")
+    @Owner("safrolov")
+    @Severity(SeverityLevel.NORMAL)
+    @Link(value = "reqres", url = "https://reqres.in")
+    @Test
+    void checkErrorTextWithMissingPassword() {
+        String authData = "{\"email\": \"eve.holt@reqres.in\"}";
+        given()
+                .body(authData)
+                .header("x-api-key", "reqres-free-v1")
+                .contentType(JSON)
+                .log().uri()
+                .when()
+                .post("/api/login")
+                .then()
+                .log().status()
+                .statusCode(400)
+                .log().body()
+                .body("error", is("Missing password"));
+    }
+
+    @DisplayName("Проверка создания пользователя")
+    @Feature("Проверка API")
+    @Owner("safrolov")
+    @Severity(SeverityLevel.NORMAL)
+    @Link(value = "reqres", url = "https://reqres.in")
+    @Test
+    void checkCreateNewUser() {
+        Faker faker = new Faker(new Locale("en"));
+        String name = faker.name().firstName();
+        String email = faker.internet().emailAddress();
+        String userData = "{\"name\": \"" + name + "\"," + "\"job\": \""+ email +"\"}";
+        given()
+                .body(userData)
+                .header("x-api-key", "reqres-free-v1")
+                .contentType(JSON)
+                .log().uri()
+                .when()
+                .post("/api/users")
+                .then()
+                .log().status()
+                .statusCode(201)
+                .log().body()
+                .body("name", is(name))
+                .body("email", is(email))
+                .body("id", is(Matchers.notNullValue()))
+                .body("createdAt", is(Matchers.notNullValue()));
+    }
+
+
+
 }
