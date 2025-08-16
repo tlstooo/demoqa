cdaa4571219337c876ac2ba65ad61ebfa0c4a3bf
tlsto <frol54@gmail.com>
2025-08-16 17:03:09 +0300

    fix checkSingleSlideMinimumCount obj

--- src/test/java/pages/MainPage.java
+++ src/test/java/pages/MainPage.java
@@ -13,7 +13,9 @@ public class MainPage {
     private final ElementsCollection
             bullets = $$(".swiper-pagination-bullet:not(.swiper-pagination-bullet-active)"),
             slides = $$(".j-big-banners-block .swiper-wrapper"),
-            singleItemsSlider = $(".j-single-banner").$$(".banners-catalog-custom__container .swiper-slide")
+            singleItemsSlider = $(".j-single-banner").$$(".banners-catalog-custom__container .swiper-slide");
+
+
     public MainPage openMainPage() {
         open(mainPageURL);
         return this;
