b6dac6ba8e2223b260fa7d18c4955facb7ac49df
tlsto <frol54@gmail.com>
2025-08-24 19:15:31 +0300

    fix "checkArticleNumber"

--- src/test/java/pages/MainPage.java
+++ src/test/java/pages/MainPage.java
@@ -15,13 +15,13 @@ public class MainPage {
     private final ElementsCollection
             bullets = $$(".swiper-pagination-bullet:not(.swiper-pagination-bullet-active)"),
             slides = $$(".j-big-banners-block .swiper-wrapper"),
-            singleItemsSlider = $$(".banners-catalog-custom__container"),
-            product = $$(".main-page__product");
+            singleItemsSlider = $$(".banners-catalog-custom__container");
 
 
     private final SelenideElement
             swiperContainer = $(".j-single-banner"),
-            article = $("#productNmId");
+            article = $("#productNmId"),
+            product = $(".main-page__product");
 
     public MainPage openMainPage() {
         open(mainPageURL);
@@ -57,7 +57,7 @@ public class MainPage {
     }
 
     public MainPage firstProductClick() {
-        product.first().click();
+        product.click();
         return this;
     }
 
