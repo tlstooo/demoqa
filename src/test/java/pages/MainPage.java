22e8ef70736991831f74822f000df00e549ca44e
tlsto <frol54@gmail.com>
2025-08-24 19:54:57 +0300

    fix "checkArticleNumber"

--- src/test/java/pages/MainPage.java
+++ src/test/java/pages/MainPage.java
@@ -16,7 +16,7 @@ public class MainPage {
             bullets = $$(".swiper-pagination-bullet:not(.swiper-pagination-bullet-active)"),
             slides = $$(".j-big-banners-block .swiper-wrapper"),
             singleItemsSlider = $$(".banners-catalog-custom__container"),
-            productCard = $$(".product-card__wrapper");
+            productCard = $$(".product-card__top-wrap");
 
 
     private final SelenideElement
