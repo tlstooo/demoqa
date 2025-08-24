91e258a1c2b882d00ee420592423d7f73f469678
tlsto <frol54@gmail.com>
2025-08-24 22:47:37 +0300

    fix "checkArticleNumber"

--- src/test/java/pages/MainPage.java
+++ src/test/java/pages/MainPage.java
@@ -15,7 +15,7 @@ public class MainPage {
     private final ElementsCollection
             bullets = $$(".swiper-pagination-bullet:not(.swiper-pagination-bullet-active)"),
             slides = $$(".j-big-banners-block .swiper-wrapper"),
-            productCard = $$("j-open-full-product-card"),
+            productCard = $$(".j-open-full-product-card"),
             singleItemsSlider = $$(".banners-catalog-custom__container");
 
 
