c91b493df6c9f7083da1a897c34915e179b1578f
tlsto <frol54@gmail.com>
2025-08-24 23:36:45 +0300

    fix "checkArticleNumber"

--- src/test/java/pages/MainPage.java
+++ src/test/java/pages/MainPage.java
@@ -5,8 +5,7 @@ import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 
-import static com.codeborne.selenide.Condition.exist;
-import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class MainPage {
@@ -65,7 +64,7 @@ public class MainPage {
     }
 
     public MainPage checkArticleIsNotNull() {
-        article.shouldNotBe(text(""));
+        article.shouldBe(not(null));
         return this;
     }
 
